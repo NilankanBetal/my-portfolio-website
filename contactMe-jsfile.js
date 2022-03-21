beedea4af68d3c049e6eab274d9d4a3bd6a9dfe7
NilankanBetal <nilankanbetal497@gmail.com>
2022-03-21 07:23:12 +0530

    added changes

---
+++ contactMe-jsfile.js
@@ -0,0 +1,13 @@
+function dynamicNavFunc() {
+    var getNavId = document.getElementById("dynamicSideNav");
+    
+    if (getNavId.style.display == "none")
+    {
+        getNavId.style.display == "block";
+    }
+
+    else
+    {
+        getNavId.style.display == "none";
+    }
+}
